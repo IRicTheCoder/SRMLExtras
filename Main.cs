e33ff9b327e6b2e8e63ff6363569946d3900c374
RicardoTheCoder <ricardo.pires@primemakers.com>
2019-07-07 02:35:22 +0100

    Added all base content to the Repo

---
+++ Main.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using HarmonyLib;
+
+namespace SRML
+{
+	/// <summary>
+	/// The main class and entry point for the mod
+	/// </summary>
+	public class Main : ModEntryPoint
+	{
+		// THE EXECUTING ASSEMBLY
+		public static Assembly execAssembly;
+
+		// PRE LOAD MOD
+		public override void PreLoad()
+		{
+			// Initializes the Console
+			Console.Init();
+
+			// Gets the Assembly being executed
+			execAssembly = Assembly.GetExecutingAssembly();
+			HarmonyInstance.PatchAll(execAssembly);
+
+			UnityEngine.SceneManagement.SceneManager.activeSceneChanged += ChangeScene;
+		}
+
+		public void ChangeScene(UnityEngine.SceneManagement.Scene old, UnityEngine.SceneManagement.Scene scene)
+		{
+			Console.Log($"{scene.buildIndex}: {scene.name ?? "NoName"}");
+		}
+
+		// POST LOAD MOD
+		public override void PostLoad()
+		{
+		}
+	}
+}
